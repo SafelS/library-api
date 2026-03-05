d0f328f782626b43bdedfb6ed10d04fb8d79c8ff
SafelS <safel_safel@outlook.de>
2026-03-05 18:09:35 +0300

    Initial commit

---
+++ src/main/java/com/library/dto/AuthorRequestDto.java
@@ -0,0 +1,10 @@
+package com.library.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthorRequestDto {
+    private String name;
+    private String bio;
+
+}
---
+++ src/main/java/com/library/dto/AuthorResponseDto.java
@@ -0,0 +1,13 @@
+package com.library.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthorResponseDto {
+    private Long id;
+    private String name;
+    private String bio;
+}
